fa32ad41739eaebdff8889d3c87ccdfbac22efe8
Mikhail Kamishov <pi_ff@mail.ru>
2023-04-12 20:09:04 +0300

    first commit

---
+++ seminar2/seminar.py
@@ -0,0 +1,8 @@
+# a = 5
+# b = 5.89
+# c = "hello"
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a,b,c))
+a = int(input("Введите первое число: "))
+b = int (input("Введите второе число: "))
+print(a, "+", b, "=", a + b)
